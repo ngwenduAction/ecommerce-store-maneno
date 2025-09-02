3c7f506def554a10c106babc369d978467a08ef3
NJ Gambu <ngwendu.action@gmail.com>
2025-09-02 14:53:09 +0200

    updates

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 // app/not-found.tsx
 export default function NotFound() {
   return (
@@ -6,12 +8,12 @@ export default function NotFound() {
       <p className="mt-4 text-lg text-gray-600">
         The page you are looking for does not exist.
       </p>
-      <a
+      <Link
         href="/"
         className="mt-6 inline-block rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
       >
         Go back home
-      </a>
+      </Link>
     </div>
   );
 }
